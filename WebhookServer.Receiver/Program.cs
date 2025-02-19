a081fda21062cfdc7a7ef2a598a7f409f0adf18a
Nguyên Huỳnh <hnn257@outlook.com>
2025-02-19 19:14:36 +0700

    Add receiver

---
+++ WebhookServer.Receiver/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Swashbuckle.Application;
+using System;
+
+[assembly: OwinStartup(typeof(WebhookServer.Receiver.Startup))]
+namespace WebhookServer.Receiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:5000/";
+
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                Console.WriteLine($"API chạy tại {baseAddress}");
+                Console.ReadLine(); // Giữ console mở
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Cấu hình Web API routes
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: new { action = RouteParameter.Optional }
+            );
+
+            // Thêm Swagger
+            config.EnableSwagger(c =>
+            {
+                c.SingleApiVersion("v1", "Console API Webhook");
+            }).EnableSwaggerUi();
+
+            // Bật Web API middleware
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ WebhookServer.Receiver/WebhookController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebhookServer.Receiver
+{
+    public class WebhookController : ApiController
+    {
+        [HttpPost]
+        [Route("api/webhook/receive")]
+        public async Task<IHttpActionResult> Receive()
+        {
+            Console.WriteLine("------------------------------------------");
+            var headers = Request.Headers.ToDictionary(k => k.Key, v => v.Value.FirstOrDefault().ToString());
+            Console.WriteLine($"Received request: {JsonConvert.SerializeObject(headers)}");
+
+            string content = await Request.Content.ReadAsStringAsync();
+            Console.WriteLine($"Received request: {content}");
+            Console.WriteLine("------------------------------------------");
+
+            return Ok(new { success = true });
+        }
+    }
+}
